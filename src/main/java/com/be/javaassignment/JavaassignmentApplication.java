5003d9d8f78ac490ab6ef1f0d038357da4801725
nikolazrnc <nikolazrnc1234@gmail.com>
2025-08-30 19:02:36 +0000

    Initial Spring Boot project

---
+++ src/main/java/com/be/javaassignment/JavaassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.be.javaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaassignmentApplication.class, args);
+	}
+
+}
